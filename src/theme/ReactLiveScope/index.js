e9de4626f45d2cc1b7d7c0083843db65ad5455db
Navneet Lal Gupta <navneetlalg@gmail.com>
2022-07-19 19:04:36 +0530

    update: added StyledBox to replace Box

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -6,6 +6,22 @@ import Radio from "@intugine-technologies/radiant/Radio";
 import Chip from "@intugine-technologies/radiant/Chip";
 import ChipDelete from "@intugine-technologies/radiant/ChipDelete";
 import { CssVarsProvider } from "@intugine-technologies/radiant";
+
+const StyledBox = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: 2,
+        alignItems: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
+
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
@@ -13,6 +29,7 @@ const ReactLiveScope = {
   CssVarsProvider,
   Button,
   Box,
+  StyledBox,
   Switch,
   Radio,
   Chip,
